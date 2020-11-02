8a1cb11e953e7ce6cd0907c605be94d0ebefcc6b
NatrixAeria <upezu@student.kit.edu>
2020-11-02 20:24:34 +0100

    Restructure workspace to seperate engine and ui

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -7,10 +7,3 @@ pub mod board;
 pub mod list;
 pub mod moves;
 pub mod threat;
-mod web;
-
-pub use board::Board;
-
-fn main() {
-    crate::web::run_webserver(Board::new());
-}
---
+++ ui/src/main.rs
@@ -0,0 +1,6 @@
+use engine::board::Board;
+use scha256_engine as engine;
+
+fn main() {
+    Board::new();
+}
